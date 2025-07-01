25c20c12927a89b5f07d83ffbdb31241abc10328
chase. <tytyupypy@gmail.com>
2025-07-02 01:48:41 +0300

    Add files via upload

---
+++ task3.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var integerVar int = 52
+	var stringVar string = "Hello, world!"
+	var boolVar bool = true
+
+	fmt.Println("int (число):", integerVar)
+	fmt.Println("string (строка):", stringVar)
+	fmt.Println("bool (булево значение:", boolVar)
+}
